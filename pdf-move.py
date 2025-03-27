154a40cdd913b14a5a262962a04f86d08e329b71
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2025-03-27 03:04:34 -0300

    Progress on pdf-move

--- pdf-move.py
+++ pdf-move.py
@@ -12,25 +12,54 @@ def main():
     global args
 
     with pikepdf.Pdf.open(args["input-file.pdf"]) as doc:
-        for page in doc.pages[4:]:
+        for page in doc.pages[4:5]:
             contents = pikepdf.parse_content_stream(page)
 
-            offset_x = -1
-            offset_y = 0
-
+            ctm = pikepdf.Matrix()  # current transformation matrix
+            text_matrix = pikepdf.Matrix()
+            stack: list[pikepdf.Matrix] = []
             result = []
             for operands, command in contents:
                 result.append([operands, command])
 
                 if command == pikepdf.Operator("Tm"):
-                    text_matrix = pikepdf.Matrix(operands).translated(
-                        offset_x, offset_y
+                    text_matrix = pikepdf.Matrix(operands)
+                elif command == pikepdf.Operator("cm"):
+                    ctm = pikepdf.Matrix(operands) @ ctm
+                elif command == pikepdf.Operator("q"):
+                    stack.append(ctm)
+                elif command == pikepdf.Operator("Q"):
+                    ctm = stack.pop()
+                elif command == pikepdf.Operator("Do"):
+                    print(page)
+
+                    # Before current item
+                    result.insert(-1, ([], pikepdf.Operator("q")))
+                    result.insert(
+                        -1,
+                        (
+                            pikepdf.Matrix()
+                            .scaled(1 / ctm.a, 1 / ctm.d)
+                            .translated(cm2point(-1), cm2point(-1))
+                            .scaled(ctm.a, ctm.d)
+                            .as_array(),
+                            pikepdf.Operator("cm"),
+                        ),
                     )
-                    result[-1][0] = pikepdf.Array(text_matrix)
+
+                    # Current item goes here
+
+                    # After current item
+                    result.append(([], pikepdf.Operator("Q")))
+                    print(result)
 
             page.Contents = doc.make_stream(pikepdf.unparse_content_stream(result))
 
         doc.save(args["output-file.pdf"])
+
+
+def cm2point(cm: float | int):
+    return cm * 28.3464566929
 
 
 def cli_args():
